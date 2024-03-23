ad2d9671ecc570c5dd0f0f92ab070f942dcde9de
harukawa-sora <interestsia@gmail.com>
2024-03-23 14:22:31 +0900

    complate

---
+++ constants/Song.ts
@@ -0,0 +1,34 @@
+export const Songs = [
+    {
+        value: 17,
+        label: "Brand New Stars!!"
+    },
+    {
+        value: 40,
+        label: "Walk with your smile"
+    },
+    {
+        value: 81,
+        label: "年越しあんさんぶる！！2020→2021"
+    },
+    {
+        value: 87,
+        label: "FUSIONIC STARS!!"
+    },
+    {
+        value: 111,
+        label: "年越しあんさんぶる！！2021→2022"
+    },
+    {
+        value: 111,
+        label: "Surprising Thanks!!"
+    },
+    {
+        value: 145,
+        label: "年越しあんさんぶる！！2022→2023"
+    },
+    {
+        value: 178,
+        label: "One with One"
+    },
+]
---
+++ constants/Unit.ts
@@ -0,0 +1,58 @@
+export const Units = [
+    {
+        label: "fine",
+        value: 2,
+    },
+    {
+        label: "Trickstar",
+        value: 1,
+    },
+    {
+        label: "유성대",
+        value: 5,
+    },
+    {
+        label: "ALKALOID",
+        value: 15,
+    },
+    {
+        label: "Eden",
+        value: 14,
+    },
+    {
+        label: "Valkyrie",
+        value: 9,
+    },
+    {
+        label: "2wink",
+        value: 7,
+    },
+    {
+        label: "Crazy:B",
+        value: 16,
+    },
+    {
+        label: "UNDEAD",
+        value: 3,
+    },
+    {
+        label: "Ra*bits",
+        value: 6,
+    },
+    {
+        label: "홍월",
+        value: 8,
+    },
+    {
+        label: "Knights",
+        value: 4,
+    },
+    {
+        label: "Switch",
+        value: 10,
+    },
+    {
+        label: "MaM",
+        value: 11,
+    },
+]
